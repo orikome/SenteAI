40a25bbaaa55a0e9be95cb9cd2654cbbbc43f764
Orikome <wyrplen@gmail.com>
2024-08-04 16:47:50 +0900

    Improve SeeingModule

--- Assets/Scripts/Module/PerceptionModule.cs
+++ Assets/Scripts/Module/PerceptionModule.cs
@@ -5,12 +5,5 @@ public abstract class PerceptionModule : AgentModule
     [SerializeField]
     protected Transform target;
     public Vector3 lastKnownLocation { get; protected set; }
-
-    protected virtual void Awake()
-    {
-        if (target == null)
-        {
-            target = Player.Instance.transform;
-        }
-    }
+    public float lastSeen;
 }
--- Assets/Scripts/Module/SeeingModule.cs
+++ Assets/Scripts/Module/SeeingModule.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SeeingModule", menuName = "Module/SeeingModule")]
@@ -9,7 +10,6 @@ public class SeeingModule : PerceptionModule
     [SerializeField]
     private LayerMask layerMask;
     public bool canSeeTarget { get; private set; }
-
     private bool previousVisibility;
 
     public override void Execute(Agent agent)
@@ -18,22 +18,25 @@ public class SeeingModule : PerceptionModule
         Vector3 directionToTarget = target.position - agent.transform.position;
         Ray ray = new Ray(agent.transform.position, directionToTarget.normalized);
 
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, range, layerMask))
+        bool hit = Physics.Raycast(ray, out RaycastHit hitInfo, range, layerMask);
+        bool targetVisible = hit && hitInfo.transform == target;
+
+        canSeeTarget = targetVisible;
+
+        // if (Time.frameCount % 20 != 0)
+        //     return;
+
+        if (targetVisible)
         {
-            canSeeTarget = hitInfo.transform == target;
-            if (canSeeTarget)
-            {
-                lastKnownLocation = target.position;
-            }
-        }
-        else
-        {
-            canSeeTarget = false;
+            lastKnownLocation = target.position;
+            lastSeen = Time.time;
         }
 
         // Reset weights when target visibility changes
         if (previousVisibility != canSeeTarget)
         {
+            lastSeen = Time.time;
+            //Debug.Log($"Target lost at: {Time.time}");
             agent.actionWeightManager.ResetWeights();
         }
 
