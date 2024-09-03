8cb737ab87fc6a8dd1f7afdf3213fbcad906728d
orikome <46493082+orikome@users.noreply.github.com>
2024-09-03 07:48:20 +0300

    Add custom editor for monitoring agent actions

---
+++ Assets/Editor/AgentActionStatus.cs
@@ -0,0 +1,59 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AgentUtilityManager))]
+public class AgentUtilityManagerInspector : Editor
+{
+    private void OnEnable()
+    {
+        EditorApplication.update += Repaint;
+    }
+
+    private void OnDisable()
+    {
+        EditorApplication.update -= Repaint;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        AgentUtilityManager manager = (AgentUtilityManager)target;
+
+        //DrawDefaultInspector();
+        //EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Action Status", EditorStyles.boldLabel);
+
+        foreach (var action in manager.actions)
+        {
+            EditorGUILayout.BeginVertical("box");
+
+            EditorGUILayout.LabelField(action.name, EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Utility Score", action.utilityScore.ToString("F2"));
+            EditorGUILayout.LabelField(
+                "Last Executed Time",
+                action.lastExecutedTime.ToString("F2") + " seconds"
+            );
+            EditorGUILayout.LabelField(
+                "Cooldown Time Remaining",
+                action.GetCooldownTimeRemaining().ToString("F2") + " seconds"
+            );
+            EditorGUILayout.LabelField(
+                "Cooldown Progress",
+                (action.GetCooldownProgress() * 100).ToString("F0") + "%"
+            );
+
+            if (action.IsOnCooldown())
+            {
+                EditorGUILayout.HelpBox("This action is currently on cooldown.", MessageType.Info);
+            }
+
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.Space();
+        }
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+    }
+}
