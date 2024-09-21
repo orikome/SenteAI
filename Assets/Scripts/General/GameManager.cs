6bef84e49508f5ef1d742e6c0961c3f244338b71
orikome <46493082+orikome@users.noreply.github.com>
2024-09-21 20:42:22 +0300

    Refactor test logic to TestManager

--- Assets/Scripts/General/GameManager.cs
+++ Assets/Scripts/General/GameManager.cs
@@ -1,24 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-[Serializable]
-public class TestData
-{
-    public int TimesTestRun;
-    public float AverageDamageDone;
-    public float AverageTimeAlive;
-    public List<RunData> Runs = new List<RunData>();
-}
-
-[Serializable]
-public class RunData
-{
-    public float DamageDone;
-    public float TimeAlive;
-}
 
 [DefaultExecutionOrder(-1000)]
 public class GameManager : MonoBehaviour
@@ -26,14 +8,10 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
     public List<Agent> activeEnemies = new();
     public Agent playerAgent;
-    private string filePath;
-    private TestData testData;
 
     void Awake()
     {
         Instance = this;
-        filePath = Path.Combine(Application.persistentDataPath, "TestData.json");
-        LoadOrInitializeMetrics();
     }
 
     void Start()
@@ -46,57 +24,14 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    void LoadOrInitializeMetrics()
-    {
-        if (File.Exists(filePath))
-        {
-            string json = File.ReadAllText(filePath);
-            testData = JsonUtility.FromJson<TestData>(json);
-        }
-        else
-        {
-            testData = new TestData();
-        }
-    }
-
     public void RestartScene()
     {
         float timeAlive = playerAgent.GetModule<HealthModule>().TimeAlive;
         PlayerMetrics playerMetrics = (PlayerMetrics)playerAgent.Metrics;
         float damageDone = playerMetrics.DamageDone;
 
-        testData.TimesTestRun++;
-        testData.Runs.Add(new RunData { DamageDone = damageDone, TimeAlive = timeAlive });
+        TestManager.Instance.SaveMetrics(damageDone, timeAlive);
 
-        float totalDamageDone = 0f;
-        float totalTimeAlive = 0f;
-        foreach (var run in testData.Runs)
-        {
-            totalDamageDone += run.DamageDone;
-            totalTimeAlive += run.TimeAlive;
-        }
-
-        testData.AverageDamageDone = totalDamageDone / testData.TimesTestRun;
-        testData.AverageTimeAlive = totalTimeAlive / testData.TimesTestRun;
-
-        DebugManager.Instance.Log(
-            $"DamageDone: {damageDone:F2} - TimeAlive: {timeAlive:F2} - AverageDamageDone: {testData.AverageDamageDone:F2} - AverageTimeAlive: {testData.AverageTimeAlive:F2}"
-        );
-
-        SaveMetricsToJson();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    void SaveMetricsToJson()
-    {
-        try
-        {
-            string json = JsonUtility.ToJson(testData, true);
-            File.WriteAllText(filePath, json);
-        }
-        catch (IOException ex)
-        {
-            Debug.LogError("Failed to write to JSON file: " + ex.Message);
-        }
     }
 }
---
+++ Assets/Scripts/General/TestManager.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[Serializable]
+public class TestData
+{
+    public int TimesTestRun;
+    public float AverageDamageDone;
+    public float AverageTimeAlive;
+    public List<RunData> Runs = new List<RunData>();
+}
+
+[Serializable]
+public class RunData
+{
+    public float DamageDone;
+    public float TimeAlive;
+}
+
+public class TestManager : MonoBehaviour
+{
+    private string filePath;
+    private TestData testData;
+
+    public static TestManager Instance { get; private set; }
+
+    void Awake()
+    {
+        Instance = this;
+
+        filePath = Path.Combine(Application.persistentDataPath, "TestData.json");
+        InitJSON();
+    }
+
+    private void InitJSON()
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            testData = JsonUtility.FromJson<TestData>(json);
+        }
+        else
+        {
+            testData = new TestData();
+        }
+    }
+
+    public void SaveMetrics(float damageDone, float timeAlive)
+    {
+        testData.TimesTestRun++;
+        testData.Runs.Add(new RunData { DamageDone = damageDone, TimeAlive = timeAlive });
+
+        float totalDamageDone = 0f;
+        float totalTimeAlive = 0f;
+        foreach (var run in testData.Runs)
+        {
+            totalDamageDone += run.DamageDone;
+            totalTimeAlive += run.TimeAlive;
+        }
+
+        testData.AverageDamageDone = totalDamageDone / testData.TimesTestRun;
+        testData.AverageTimeAlive = totalTimeAlive / testData.TimesTestRun;
+
+        DebugManager.Instance.Log(
+            $"DamageDone: {damageDone:F2} - TimeAlive: {timeAlive:F2} - AverageDamageDone: {testData.AverageDamageDone:F2} - AverageTimeAlive: {testData.AverageTimeAlive:F2}"
+        );
+
+        SaveMetricsToJSON();
+    }
+
+    private void SaveMetricsToJSON()
+    {
+        try
+        {
+            string json = JsonUtility.ToJson(testData, true);
+            File.WriteAllText(filePath, json);
+        }
+        catch (IOException ex)
+        {
+            Debug.LogError("Failed to write to JSON file: " + ex.Message);
+        }
+    }
+}
